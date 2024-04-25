29c164de24efbbdc99275570e1ebac7fe8ea6c29
gina2208 <126706562+gina2208@users.noreply.github.com>
2024-04-25 11:48:43 -0500

    Hice la vista principa gerente

--- gerente/urls.py
+++ gerente/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import iniciar_sesion, mostrar_empleados
+from .views import iniciar_sesion, mostrar_empleados,reportes
 
 urlpatterns = [
     path('iniciar_sesion/', iniciar_sesion, name='iniciar_sesion'),
+    path('reportes/', reportes, name='reportes'),
     path('mostrar_empleados/', mostrar_empleados, name='mostrar_empleados'),
 ]
 
--- gerente/views.py
+++ gerente/views.py
@@ -12,7 +12,7 @@ def iniciar_sesion(request):
                 empleado_existente = empleado.objects.get(cedula=cedula)  # Busca el empleado por cédula
                 if empleado_existente.contrasena == contrasena:
                     if empleado_existente.FK_ID_Rol_id == 0: #INICIO GERENTE
-                        return redirect('mostrar_empleados')  # Redirige si la contraseña coincide
+                        return redirect('reportes')  # Redirige si la contraseña coincide
                     elif empleado_existente.FK_ID_Rol_id == 1: #INICIO ADMINISTRADOR
                         return redirect('Vista_Admin')  # Redirige si la contraseña coincide
                     elif empleado_existente.FK_ID_Rol_id == 2: #INICIO PREPARADOR
@@ -26,7 +26,8 @@ def iniciar_sesion(request):
     else:
         form = LoginForm()
     return render(request, 'iniciar_sesion.html', {'form': form})
-
+def reportes(request):
+    return render(request, 'reportes.html')
 def mostrar_empleados(request):
     empleados = empleado.objects.all()
     return render(request, 'mostrar_empleados.html', {'empleados': empleados})
