c1225f0b24ab7560d2f324e3894de6fadd969d09
mguzguz <camiguzmanbello@gmail.com>
2024-04-26 10:04:16 -0500

    mejore los campos de registro administrador

--- gerente/forms.py
+++ gerente/forms.py
@@ -8,4 +8,14 @@ class EmpleadoForm(forms.ModelForm):
     class Meta:
         model = empleado
         fields = ['cedula', 'nom_emp', 'correo_emp', 'contrasena', 'tel_emp', 'direc_emp', 'ciudad_emp', 'foto_emp', 'FK_ID_Rol']
-        
+        widgets = {
+            'cedula': forms.TextInput(attrs={'class': 'form-control'}),
+            'nom_emp': forms.TextInput(attrs={'class': 'form-control'}),
+            'correo_emp': forms.EmailInput(attrs={'class': 'form-control'}),
+            'contrasena': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'tel_emp': forms.TextInput(attrs={'class': 'form-control'}),
+            'direc_emp': forms.TextInput(attrs={'class': 'form-control'}),
+            'ciudad_emp': forms.TextInput(attrs={'class': 'form-control'}),
+            'foto_emp': forms.FileInput(attrs={'class': 'form-control'}),
+            'FK_ID_Rol': forms.HiddenInput(attrs={'value': '1'}),  # Si deseas mantener este campo oculto
+        }
