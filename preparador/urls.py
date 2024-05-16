a852256744f316c32bd54edba176cf7cbba39b71
mguzguz <camiguzmanbello@gmail.com>
2024-05-16 16:33:46 -0500

    preparador modificar

--- preparador/urls.py
+++ preparador/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import  pedidos, inventariopreparador
+from .views import  pedidos, inventariopreparador, modificar_ingrediente
 
 urlpatterns = [
     path('pedidos/', pedidos, name='pedidos'),
     path('inventariopreparador/', inventariopreparador, name='inventariopreparador'),
+    path('modificar_ingrediente/', modificar_ingrediente, name='modificar_ingrediente'),
+
 ]
