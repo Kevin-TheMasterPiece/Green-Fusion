51567388ea4dddea6458d6fcc7851e60ce94fdda
kevin <kevin01517ar@gmail.com>
2024-05-17 00:01:16 -0500

    agregue vista de pedidos al preparador

--- preparador/urls.py
+++ preparador/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import  pedidos, inventariopreparador, modificar_ingrediente
 
+
 urlpatterns = [
     path('pedidos/', pedidos, name='pedidos'),
     path('inventariopreparador/', inventariopreparador, name='inventariopreparador'),
     path('modificar_ingrediente/', modificar_ingrediente, name='modificar_ingrediente'),
+    path('<int:pedido_id>/', pedidos, name='pedido_detalle')
 
 ]
--- preparador/views.py
+++ preparador/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from administrador.models import ingrediente
 from django.contrib import messages
+from store.models import factura, ensaladas
 # Create your views here.
-def pedidos (request):
-    return render(request,'pedidos.html')
+def pedidos (request, pedido_id= None):
+    pedido = None
+    pedidos = factura.objects.filter(preparada=False)
+    ensaladas_pedido = []
+    ingredientes = []
+
+    for pedid in pedidos:
+        for ensalada in pedid.items.all():
+            ensaladas_pedido.append(ensalada)
+            ingredientes.extend(ensalada.FK_ensalada.ingredientes.all())
+
+    total_ensaladas = len(ensaladas_pedido)
+
+    if pedido_id:
+         pedido = factura.objects.filter(id=pedido_id)
+    
+    return render(request, 'pedidos.html', {
+        'pedidos': pedidos,
+        'ensaladas_pedido': ensaladas_pedido,
+        'total_ensaladas': total_ensaladas,
+        'ingredientes': ingredientes,
+        'pedido': pedido,
+    })
+
+
+
+
+
 def inventariopreparador (request):
     ingredientes = ingrediente.objects.all()
     return render(request,'inventariopreparador.html', {'ingredientes': ingredientes})
